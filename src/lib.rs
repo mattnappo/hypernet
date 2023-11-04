064f1f6e47a7a3c1a82ac36dc23ee8560a3b4c05
Matt Nappo <mattnappo@gmail.com>
2023-11-04 16:56:30 -0400

    Added Hypernodes

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,50 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    hash::{Hash, Hasher},
+    net::Ipv4Addr,
+    sync::{Arc, Mutex},
+};
+
+#[derive(Debug)]
+struct Hypernode<T: Default> {
+    address: Option<Ipv4Addr>,
+    id: u32,
+    data: Arc<Mutex<T>>,
+}
+
+impl<T: Default> Hash for Hypernode<T> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.id.hash(state)
+    }
+}
+
+impl<T: Default> PartialEq for Hypernode<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+
+impl<T: Default> Eq for Hypernode<T> {}
+
+impl<T: Default> Hypernode<T> {
+    pub fn new(id: u32) -> Self {
+        Self {
+            address: None,
+            id,
+            data: Arc::new(Mutex::new(T::default())),
+        }
+    }
+
+    pub fn start() {
+        // Needs to listen for connections and handle them
+    }
+}
 
 /// A d-dimensional cube
 #[derive(Debug)]
-struct Hypercube {
+struct Hypercube<T: Default> {
     /// Connections between nodes
-    graph: HashMap<u32, HashSet<u32>>,
+    graph: HashMap<u32, HashSet<Hypernode<T>>>,
 
     /// Dimension of the hypercube
     d: u32,
@@ -13,7 +53,7 @@ struct Hypercube {
     n: u32,
 }
 
-impl Hypercube {
+impl<T: Default> Hypercube<T> {
     /// Make a `Hypercube` of dimension `d`
     pub fn new(d: u32) -> Self {
         // Make the Hypercube id mapping
@@ -24,7 +64,7 @@ impl Hypercube {
                 graph
                     .entry(i)
                     .or_insert(HashSet::new())
-                    .insert(i ^ 2u32.pow(k));
+                    .insert(Hypernode::new(i ^ 2u32.pow(k)));
             })
         });
 
@@ -34,6 +74,9 @@ impl Hypercube {
             n: 2u32.pow(d),
         }
     }
+
+    /// Send data to all nodes, from an id
+    pub fn broadcast(&self, from: u32, data: T) {}
 }
 
 #[cfg(test)]
@@ -41,9 +84,15 @@ pub mod tests {
     use super::*;
     #[test]
     fn test_new() {
-        let cube = Hypercube::new(3);
+        let cube: Hypercube<()> = Hypercube::new(3);
+
+        let m: HashMap<u32, HashSet<u32>> = cube
+            .graph
+            .into_iter()
+            .map(|(k, v)| (k, v.into_iter().map(|a| a.id).collect::<HashSet<u32>>()))
+            .collect();
         assert_eq!(
-            cube.graph,
+            m,
             HashMap::from([
                 (0b000, HashSet::from([0b001, 0b010, 0b100])),
                 (0b001, HashSet::from([0b000, 0b011, 0b101])),
